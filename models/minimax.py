ce8a8ab85331e0b9ebc41b7ef1b0bbe997d39132
NghiemNgocDuc <161023401+NghiemNgocDuc@users.noreply.github.com>
2025-10-17 15:22:03 -0400

    Update minimax.py

--- models/minimax.py
+++ models/minimax.py
@@ -1,6 +1,6 @@
 import copy
 
-# 🐚 Game Setup
+
 def initialize_board():
     return [["quan"], ["dan"] * 5, ["dan"] * 5, ["dan"] * 5, ["dan"] * 5, ["dan"] * 5,
             ["dan"] * 5, ["dan"] * 5, ["dan"] * 5, ["dan"] * 5, ["dan"] * 5, ["quan"]]
